ff18047d0e607cc4c0fac14d6e541d0c05302ac2
PJ Reiniger <pj.reiniger@gmail.com>
2025-01-15 19:14:37 -0500

    Patch opencv

--- libraries/bzlmodRio-opencv/generate/get_opencv_dependencies.py
+++ libraries/bzlmodRio-opencv/generate/get_opencv_dependencies.py
@@ -52,7 +52,7 @@ def _default_all_platforms():
 def get_opencv_dependencies():
     year = "2025"
     version = "4.10.0-3"
-    patch = ""
+    patch = ".bcr1"
 
     group_id = f"edu.wpi.first.thirdparty.frc{year}.opencv"
 
