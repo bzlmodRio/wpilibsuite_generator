03a7981ce6288f7e338faf2c415ae42489b05264
PJ Reiniger <pj.reiniger@gmail.com>
2026-02-14 00:06:07 -0500

    More

--- generate/generate.py
+++ generate/generate.py
@@ -6,10 +6,13 @@ def main():
     r = runfiles.Create()
     generators = [
         r.Rlocation("bzlmodrio-ni-gentool+/generate"),
-        r.Rlocation("bzlmodrio-opencv-gentool+/generate")
+        r.Rlocation("bzlmodrio-opencv-gentool+/generate"),
+        # r.Rlocation("rules_bzlmodrio_toolchains-gentool+/generate"),
+        # r.Rlocation("rules_bzlmodrio_jdk-gentool+/generate"),
     ]
 
     for gen in generators:
+        print(gen)
         subprocess.check_call([gen])
 
 if __name__ == "__main__":
